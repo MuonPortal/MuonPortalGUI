bca32287295a4c1e2cb89c9d7dda21d58665ddf2
riggi <simone.riggi@gmail.com>
2018-07-26 18:16:59 +0200

    Add dictionary file

---
+++ ViewerLinkDef.h
@@ -0,0 +1,16 @@
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link off all typedef;
+
+#pragma link C++ class POCAImageViewer+;
+#pragma link C++ class EMLLImageViewer+;
+#pragma link C++ class ACFImageViewer+;
+#pragma link C++ class ClusteringImageViewer+;
+
+//#pragma link C++ class MyTASImage-; 
+#pragma link C++ class TASImageNew+; 
+#endif
+
